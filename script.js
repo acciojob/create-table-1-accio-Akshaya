c848e8185c091c9c9decccf250d62a73916e376d
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-29 01:17:04 -0800

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function insert_Row() {
 	const cell1=newRow.insertCell(0);
 	const cell2=newRow.insertCell(1);
 
-	cell1.textContent = "Row3 cell1"
-	cell2.textContent = "Row4 cell2"
+	cell1.textContent = "New Cell1";
+	cell2.textContent = "New Cell2";
+
+	console.log("New row added. Current table rows:", table.rows);
   
 }
